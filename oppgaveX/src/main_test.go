b63b91c3fc4aba3da53429922b335ed65f23e2d9
theSkibb <kristianskibrek@protonmail.com>
2024-11-28 20:11:21 +0100

    add tests for checkline

--- oppgaveX/src/main_test.go
+++ oppgaveX/src/main_test.go
@@ -32,10 +32,55 @@ func TestGetPackageInfoFailsForMissingVersion(t *testing.T) {
 
 // if a package is missing the name, the getPackageList function should fail
 func TestGetPackageListFailsForMissingPackageName(t *testing.T) {
+
 	result, err := getPackageList("./Packages_missing_name")
 
 	// function should have returned error
 	if err == nil || len(result) != 0 {
 		t.Fatal("getPackageList with Packages_missing_name should return with error and empty list")
+	}
+}
+
+func TestCheckLinePackageNamePackage(t *testing.T) {
+	line := []byte("Package: 3depict")
+	checkingFor := "Package: "
+	wantedResult := "3depict"
+	result := checkLine(checkingFor, line)
+
+	if result != wantedResult {
+		t.Fatalf("checkline should return %s checking %s for %s", wantedResult, line, checkingFor)
+	}
+}
+
+func TestCheckLinePackageVersion(t *testing.T) {
+	line := []byte("Version: 1.0-3.3")
+	checkingFor := "Version: "
+	wantedResult := "1.0-3.3"
+	result := checkLine(checkingFor, line)
+
+	if result != wantedResult {
+		t.Fatalf("checkline should return %s checking %s for %s", wantedResult, line, checkingFor)
+	}
+}
+
+func TestCheckLinePackageNamePackageNegative(t *testing.T) {
+	line := []byte("test Package: some name")
+	checkingFor := "Package: "
+	wantedResult := ""
+	result := checkLine(checkingFor, line)
+
+	if result != wantedResult {
+		t.Fatalf("checkline should return an empty string checking %s for %s", line, checkingFor)
+	}
+}
+
+func TestCheckLinePackageVersionNegative(t *testing.T) {
+	line := []byte("Python-Version: ,")
+	checkingFor := "Version: "
+	wantedResult := ""
+	result := checkLine(checkingFor, line)
+
+	if result != wantedResult {
+		t.Fatalf("checkline should return a empty string checking %s for %s", line, checkingFor)
 	}
 }
